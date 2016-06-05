f830f687bbef6aec2645b320de5520e29c00ff27
THEZAKCZ <fivecz15@gmail.com>
2016-06-05 13:41:30 +0200

    ok

--- src/main/java/EssentialsPE/Commands/Heal.java
+++ src/main/java/EssentialsPE/Commands/Heal.java
@@ -10,29 +10,29 @@ import cn.nukkit.utils.TextFormat;
 abstract class Heal extends BaseCommand{
 
 	public Heal(BaseAPI api){
-        super(api, "heal", "Heal yourself or other player", "[player]");
+        super(api, "heal", "Vylecit sebe, nebo ostatni hrace", "[player]");
         this.setPermission("essentials.heal.use");
     }
 
-    public boolean execute(CommandSender sender, String alias, String[] args) {
-        if(!this.testPermission(sender)){
-            return false;
-        }
-        if((!(args.length == 1)) && !(sender instanceof Player) || (args.length) > 1) {
-            this.sendUsage(sender, alias);
-            return false;
-        }
-        Player player = (Player)sender;
-		if(!(args.length == 1 && !(sender.getServer().getOfflinePlayer(args[0]).isOnline()))) {
-            sender.sendMessage(TextFormat.RED + "[Error] Player not found");
-            return false;
-        }
-        player.setHealth(20);
-        player.getLevel().addParticle(new HeartParticle(player.add(0, 2, 0)));
-        player.sendMessage(TextFormat.GREEN + "You have been healed!");
-        if(player == sender){
-            sender.sendMessage(TextFormat.GREEN + player.getDisplayName() + " has been healed!");
-        }
-        return true;
-    }
-}
+	  public boolean execute(CommandSender sender, String alias, String[] args) {
+	      if(!this.testPermission(sender)){
+	          return false;
+	      }
+	      if((!(args.length == 0)) && !(sender instanceof Player) || (args.length) > 1) {
+	    	  this.sendUsage(sender, alias);
+	          return false;
+	      }
+	      Player player = (Player)sender;
+			if((args.length == 1 && !(sender.getServer().getOfflinePlayer(args[0]).isOnline()))) {
+	          sender.sendMessage(TextFormat.RED + "[Chyba] Hac neni online");
+	          return false;
+	      }
+	      player.setHealth(20);
+	      player.getLevel().addParticle(new HeartParticle(player.add(0, 2, 0)));
+	      player.sendMessage(TextFormat.GREEN + "Byl jsi vylecen!");
+	      if(args.length == 1 && !(sender.getServer().getOfflinePlayer(args[0]).isOnline())){
+	          sender.sendMessage(TextFormat.GREEN + player.getDisplayName() + " byl vylecen!");
+	      }
+	      return true;
+	  }
+	}
