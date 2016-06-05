e00faecbb6d45656e0d784af5371a938aaf18d0c
THEZAKCZ <fivecz15@gmail.com>
2016-06-05 14:48:03 +0200

    funguje

--- src/main/java/EssentialsPE/Commands/Heal.java
+++ src/main/java/EssentialsPE/Commands/Heal.java
@@ -10,7 +10,7 @@ import cn.nukkit.utils.TextFormat;
 abstract class Heal extends BaseCommand{
 
 	public Heal(BaseAPI api){
-        super(api, "heal", "Vylecit sebe, nebo ostatni hrace", "[player]");
+        super(api, "heal", "Heal yourself or other player", "[player]");
         this.setPermission("essentials.heal.use");
     }
 
@@ -24,15 +24,18 @@ abstract class Heal extends BaseCommand{
 	      }
 	      Player player = (Player)sender;
 			if((args.length == 1 && !(sender.getServer().getOfflinePlayer(args[0]).isOnline()))) {
-	          sender.sendMessage(TextFormat.RED + "[Chyba] Hac neni online");
+	          sender.sendMessage(TextFormat.RED + "[Chyba] Hrac neni online");
 	          return false;
-	      }
+			} else if(args.length == 1) {
+				player = sender.getServer().getPlayer(args[0]);
+			}
+			
 	      player.setHealth(20);
 	      player.getLevel().addParticle(new HeartParticle(player.add(0, 2, 0)));
 	      player.sendMessage(TextFormat.GREEN + "Byl jsi vylecen!");
-	      if(args.length == 1 && !(sender.getServer().getOfflinePlayer(args[0]).isOnline())){
+	      if(args.length == 1){
 	          sender.sendMessage(TextFormat.GREEN + player.getDisplayName() + " byl vylecen!");
 	      }
 	      return true;
-	  }
-	}
+			}
+	  	}
