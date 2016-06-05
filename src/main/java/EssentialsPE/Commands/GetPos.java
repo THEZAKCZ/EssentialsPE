75b51c08a72564de73280d57bcd96df0ee8a2ff7
THEZAKCZ <fivecz15@gmail.com>
2016-06-05 15:03:32 +0200

    funguje

--- src/main/java/EssentialsPE/Commands/GetPos.java
+++ src/main/java/EssentialsPE/Commands/GetPos.java
@@ -18,7 +18,6 @@ abstract class GetPos extends BaseCommand{
             return false;
         }
         if((args.length == 0) && !(sender instanceof Player) || (args.length > 1)){
-        	this.sendUsage(sender, alias);
             return false;
         }
         Player player = (Player)sender;
@@ -30,9 +29,16 @@ abstract class GetPos extends BaseCommand{
         		if((args.length == 1 && !(sender.getServer().getOfflinePlayer(args[0]).isOnline()))) {
                   sender.sendMessage(TextFormat.RED + "[Chyba] Hrac neni online");
                 return false;
+      		} else if(args.length == 1) {
+      			player = sender.getServer().getPlayer(args[0]);
+      		}
             }
-        }
-        sender.sendMessage(TextFormat.GREEN + (player == sender ? "Jsi " : player.getDisplayName() + TextFormat.GRAY + "je ") + "ve svete: " + TextFormat.AQUA + player.getLevel().getName() + "\n" + TextFormat.GREEN + "Souradnice: " + TextFormat.YELLOW + "X: " + TextFormat.AQUA + player.getFloorX() + TextFormat.GREEN + ", " + TextFormat.YELLOW + "Y: " + TextFormat.AQUA + player.getFloorY() + TextFormat.GREEN + ", " + TextFormat.YELLOW + "Z: " + TextFormat.AQUA + player.getFloorZ());
+        if(args.length == 0 ) {
+        sender.sendMessage(TextFormat.GREEN + "Jsi ve svete: " + TextFormat.AQUA + player.getLevel().getName() + "\n" + TextFormat.GREEN + "Souradnice: " + TextFormat.YELLOW + "X: " + TextFormat.AQUA + player.getFloorX() + TextFormat.GREEN + ", " + TextFormat.YELLOW + "Y: " + TextFormat.AQUA + player.getFloorY() + TextFormat.GREEN + ", " + TextFormat.YELLOW + "Z: " + TextFormat.AQUA + player.getFloorZ());
         return true;
+        	} else if(args.length == 1 ) {
+              sender.sendMessage(TextFormat.GREEN + "Hrac " + TextFormat.AQUA + player.getName() + TextFormat.GREEN + " je ve svete: " + TextFormat.AQUA + player.getLevel().getName() + "\n" + TextFormat.GREEN + "Souradnice: " + TextFormat.YELLOW + "X: " + TextFormat.AQUA + player.getFloorX() + TextFormat.GREEN + ", " + TextFormat.YELLOW + "Y: " + TextFormat.AQUA + player.getFloorY() + TextFormat.GREEN + ", " + TextFormat.YELLOW + "Z: " + TextFormat.AQUA + player.getFloorZ());
         }
+  	return true;
     }
+  }
